e5b36b3cfa5e763098fc30d068e508444aaaf67c
AwAkA <rizkyfebrian.hidayat@gmail.com>
2024-06-07 09:08:24 +0700

    Update Class Page

---
+++ src/main/java/com/gc_project/Controllers/ClassController.java
@@ -0,0 +1,15 @@
+package com.gc_project.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequestMapping ("/Class")
+public class ClassController {
+    @GetMapping
+    public String welcome(){
+        return "Class";
+    }
+}
