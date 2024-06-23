b6efca890829d375e519216d6a398947b9608b4c
AwAkA <rizkyfebrian.hidayat@gmail.com>
2024-06-23 21:27:17 +0700

    Update login di Appcontroller

--- src/main/java/com/gc_project/AppController.java
+++ src/main/java/com/gc_project/AppController.java
@@ -38,4 +38,10 @@ public class AppController {
     public String viewUsersList() {
         return "users"; // Pastikan users.html ada di templates
     }
+
+    // Redirect ke home setelah login berhasil
+    @PostMapping("/login")
+    public String login() {
+        return "redirect:/index";
+    }
 }
