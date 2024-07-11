b849c289b736000679195a3297f35e6433091544
Mouse38271 <68421057+Mouse38271@users.noreply.github.com>
2024-07-11 23:24:35 +0700

    Menambahkan fitur

--- src/main/java/com/gc_project/PasswordEncoder.java
+++
@@ -1,9 +0,0 @@
-package com.gc_project;
-
-public class PasswordEncoder {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
--- src/main/java/com/gc_project/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gc_project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
